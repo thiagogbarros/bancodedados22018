abec573dce54a03a91b37f33d45cd172488cf6d1
Thiago <thiagobarros@cjr.org.br>
2018-12-02 15:33:30 -0200

    Exportando o template utilizando o Templates helper, porem sem testar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( vendor/bootstrap/css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( vendor/fontawesome-free/css/all.min.css )
+Rails.application.config.assets.precompile += %w( https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic )
+Rails.application.config.assets.precompile += %w( https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800 )
+Rails.application.config.assets.precompile += %w( css/clean-blog.min.css )
+Rails.application.config.assets.precompile += %w( vendor/jquery/jquery.min.js )
+Rails.application.config.assets.precompile += %w( vendor/bootstrap/js/bootstrap.bundle.min.js )
+Rails.application.config.assets.precompile += %w( js/clean-blog.min.js )
+Rails.application.config.assets.precompile += %w( *.jpg *.png )
