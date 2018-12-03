c913ddffeaba85d9d9a3331edefb862a3ef1f9f0
Thiago <thiagobarros@cjr.org.br>
2018-12-03 11:24:09 -0200

    Criando controller static_pages e colocando o template na home

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   resources :scales
   resources :powers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#home'
 end
