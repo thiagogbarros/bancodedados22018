39f0513b6c639fbad20e3e2c5cec463f1ed858c2
Thiago <thiagobarros@cjr.org.br>
2018-12-03 10:28:33 -0200

    Criando o seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,117 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_141559) do
+ActiveRecord::Schema.define(version: 2018_12_03_121244) do
 
-  create_table "news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "communications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "fake_news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.bigint "font_id"
+    t.bigint "scale_id"
+    t.bigint "population_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["font_id"], name: "index_fake_news_on_font_id"
+    t.index ["population_id"], name: "index_fake_news_on_population_id"
+    t.index ["scale_id"], name: "index_fake_news_on_scale_id"
+  end
+
+  create_table "fonts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "news_communications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "fake_news_id"
+    t.bigint "communication_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["communication_id"], name: "index_news_communications_on_communication_id"
+    t.index ["fake_news_id"], name: "index_news_communications_on_fake_news_id"
+  end
+
+  create_table "news_people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "fake_news_id"
+    t.bigint "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fake_news_id"], name: "index_news_people_on_fake_news_id"
+    t.index ["person_id"], name: "index_news_people_on_person_id"
+  end
+
+  create_table "news_states", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "fake_news_id"
+    t.bigint "state_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fake_news_id"], name: "index_news_states_on_fake_news_id"
+    t.index ["state_id"], name: "index_news_states_on_state_id"
+  end
+
+  create_table "offices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.bigint "power_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["power_id"], name: "index_offices_on_power_id"
+  end
+
+  create_table "parties", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "office_id"
+    t.bigint "party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["office_id"], name: "index_people_on_office_id"
+    t.index ["party_id"], name: "index_people_on_party_id"
+  end
+
+  create_table "populations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "powers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scales", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "states", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "fake_news", "fonts"
+  add_foreign_key "fake_news", "populations"
+  add_foreign_key "fake_news", "scales"
+  add_foreign_key "news_communications", "communications"
+  add_foreign_key "news_communications", "fake_news"
+  add_foreign_key "news_people", "fake_news"
+  add_foreign_key "news_people", "people"
+  add_foreign_key "news_states", "fake_news"
+  add_foreign_key "news_states", "states"
+  add_foreign_key "offices", "powers"
+  add_foreign_key "people", "offices"
+  add_foreign_key "people", "parties"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,116 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Power.destroy_all
+
+Power.create!([{
+    description: 'Legislativo'
+  },
+  {
+    description: 'Executivo'
+  }
+])
+
+puts "Created #{Power.count} movies"
+
+Scale.destroy_all
+Scale.create!([{
+    description: 'Nacional'
+  },
+  {
+    description: 'Internacional'
+  }
+])
+
+puts "Created #{Scale.count} scales"
+
+Font.destroy_all
+
+Font.create!([{
+    description: 'Desconhecida'
+  },
+  {
+    description: 'Folha de São Paulo'
+  },
+  {
+    description: 'O Globo'
+  },
+  {
+    description: 'Veja'
+  },
+  {
+    description: 'Carta Capital'
+  }
+])
+
+puts "Created #{Font.count} fonts"
+
+Party.destroy_all
+Party.create!([{
+    description: 'Movimento Democrático Brasileiro'
+  },
+  {
+    description: 'Partido Trabalhista Brasileiro'
+  },
+  {
+    description: 'Partido dos Trabalhadoes'
+  },
+  {
+    description: 'Partido da Social Democracia Brasileira'
+  },
+  {
+    description: 'Partido Social Liberal'
+  },
+  {
+    description: 'Partido Novo'
+  }
+])
+
+puts "Created #{Party.count} parties"
+
+Communication.destroy_all
+
+Communication.create!([{
+    description: 'Whatsapp'
+  },
+  {
+    description: 'Facebook'
+  },
+  {
+    description: 'Jornal'
+  },
+  {
+    description: 'Televisão'
+  },
+  {
+    description: 'Revista'
+  },
+  {
+    description: 'Instagram'
+  }
+])
+
+puts "Created #{Communication.count} communications"
+
+Population.destroy_all
+
+Population.create!([{
+description: 'Classe Baixa'
+},
+{
+description: 'Classe Média Baixa'
+},
+{
+description: 'Classe Média'
+},
+{
+description: 'Classe Média Alta'
+},
+{
+description: 'Classe Alta'
+}
+])
+
+puts "Created #{Population.count} Populations"
